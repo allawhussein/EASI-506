357d7538a716bf9e46cacc14e2166ab3a004d963
Hussein Allaw <allawhussein@gmail.com>
2024-11-09 16:16:26 +0200

    implemented ComputerBatterySensor The implementation is splitted in a client\server style where node is the client and a powershell script is the server

---
+++ JS-TS-SensorHub/server/sensors/ComputerSensorServer.ps1
@@ -0,0 +1,69 @@
+# Function to listen for incoming connections
+function ListenForConnections {
+    param (
+        [string]$IPAddress = "127.0.0.1",
+        [int]$Port = 8080
+    )
+
+    # Create a socket object
+    $listener = New-Object System.Net.Sockets.TcpListener($IPAddress, $Port)
+
+    Write-Host "Server listening on $IPAddress :$Port..."
+    $listener.Start()
+    while ($true) {
+        try {
+            # Start waiting for a connection
+            $clientSocket = $listener.AcceptTcpClient()
+
+            # Get the client IP address and port
+            $remoteIP = $clientSocket.Client.RemoteEndPoint.Address
+            $remotePort = $clientSocket.Client.RemoteEndPoint.Port
+
+            Write-Host "New connection from $remoteIP :$remotePort"
+
+            # Read data from the client
+            $stream = $clientSocket.GetStream()
+            $buffer = New-Object byte[] 1024
+            $bytesRead = $null
+
+            $bytesRead = $stream.Read($buffer, 0, $buffer.Length)
+            $message = [System.Text.Encoding]::ASCII.GetString($buffer, 0, $bytesRead)
+            Write-Host "Received: $message"
+
+            # Send response back to the client
+            if ($message -eq "batteryPercentage") {
+                Write-Host "Getting battery percentage"
+                $response = iex((Get-WmiObject win32_battery).estimatedChargeRemaining)
+            } elseif ($message -eq "batteryCount") {
+                Write-Host "Getting battery count"
+                $response = iex((Get-WmiObject win32_battery).Count)
+            } elseif ($message -eq "cpuTemperature") {
+                Write-Host "Getting CPU temperature"
+                $response = iex((Get-WmiObject MSAcpi_ThermalZoneTemperature -Namespace "root/wmi").CurrentTemperature[0])
+            } else {
+                $response = "error"
+            }
+            Write-Host "Operation Result $response"
+
+            $byteArray = [System.Text.Encoding]::ASCII.GetBytes($response)
+            $stream.Write($byteArray, 0, $byteArray.Length)
+            $stream.Flush()
+
+            Write-Host "Response sent to $remoteIP :$remotePort"
+
+            # Close the client socket
+            $clientSocket.Close()
+        }
+        catch {
+            Write-Host "Exception: $_"
+        }
+    }
+
+    # Close the listener when done
+    finally {
+        $listener.Stop()
+    }
+}
+
+# Call the function to start listening
+ListenForConnections
